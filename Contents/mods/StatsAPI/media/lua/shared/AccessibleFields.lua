0da68c12d677803d33de729615284a67947e6380
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2023-09-26 21:12:13 +0100

    update accessible fields

--- Contents/mods/StatsAPI/media/lua/shared/AccessibleFields.lua
+++ Contents/mods/StatsAPI/media/lua/shared/AccessibleFields.lua
@@ -1,6 +1,4 @@
 -- this is added to the same directory as in the main mod so that it doesn't matter which one overrides the other
-local bannedClasses = {ArrayList.class, Stack.class}
-
 local getNumClassFields = getNumClassFields
 local getClassField = getClassField
 local getClassFieldVal = getClassFieldVal
@@ -9,35 +7,47 @@ local match = string.match
 local tostring = tostring
 local pairs = pairs
 local __classmetatables = __classmetatables
+local type = type
 
-for i = 1, #bannedClasses do
-    bannedClasses[bannedClasses[i]] = true
-    bannedClasses[i] = nil
-end
+local classtables = {}
 
-for class, metatable in pairs(__classmetatables) do
-    if not bannedClasses[class] and metatable.__index and type(metatable.__index) == "table" then -- something weird in here breaks it, Vector's exposure seems to be bugged?
-        local metaMetatable = {}
-        
-        local getField = function(self, key)
-            local fieldGetter = metaMetatable.fieldGetters[key]
-            if fieldGetter then
-                return fieldGetter(self)
+---@param t table
+local function addClassesRecurse(t)
+    for _,v in pairs(t) do
+        if type(v) == "table" then
+            local class = v.class
+            if class then
+                table.insert(classtables, __classmetatables[class].__index)
+            else
+                addClassesRecurse(v)
             end
         end
-        
-        metaMetatable.__index = function(self, key)
-            local fieldGetters = {}
-            for i = 0, getNumClassFields(self)-1 do
-                local field = getClassField(self, i)
-                local fieldName = match(tostring(field), "([^%.]+)$")
-                fieldGetters[fieldName] = function(self) return getClassFieldVal(self, field) end
-            end
-            metaMetatable.fieldGetters = fieldGetters
-            metaMetatable.__index = getField
-            return self[key]
-        end
-        
-        setmetatable(metatable.__index, metaMetatable)
     end
+end
+addClassesRecurse(zombie)
+
+for i = 1, #classtables do
+    local classtable = classtables[i]
+    local metatable = {}
+    
+    local getField = function(self, key)
+        local fieldGetter = metatable.fieldGetters[key]
+        if fieldGetter then
+            return fieldGetter(self)
+        end
+    end
+    
+    metatable.__index = function(self, key)
+        local fieldGetters = {}
+        for i = 0, getNumClassFields(self)-1 do
+            local field = getClassField(self, i)
+            local fieldName = match(tostring(field), "([^%.]+)$")
+            fieldGetters[fieldName] = function(self) return getClassFieldVal(self, field) end
+        end
+        metatable.fieldGetters = fieldGetters
+        metatable.__index = getField
+        return self[key]
+    end
+    
+    setmetatable(classtable, metatable)
 end
