d749003807a9df5c8771aa2ff789282ff0484678
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2023-10-18 19:17:25 +0100

    fix incorrect moodletype mapping

--- Contents/mods/StatsAPI/media/lua/client/StatsAPI/overrides/Moodles.lua
+++ Contents/mods/StatsAPI/media/lua/client/StatsAPI/overrides/Moodles.lua
@@ -1,30 +1,30 @@
 local CharacterStats = require "StatsAPI/CharacterStats"
 
 local moodleTypeToLuaMoodle = {
-    [0] = "endurance",
-    [1] = "tired",
-    [2] = "hungry",
-    [3] = "panic",
-    [4] = "sick",
-    [5] = "bored",
-    [6] = "unhappy",
-    [7] = "bleeding",
-    [8] = "wet",
-    [9] = "hasacold",
-    --[10] = "angry",
-    [11] = "stress",
-    [12] = "thirst",
-    [13] = "injured",
-    [14] = "pain",
-    [15] = "heavyload",
-    [16] = "drunk",
-    [17] = "dead",
-    [18] = "zombie",
-    [19] = "hyperthermia",
-    [20] = "hypothermia",
-    [21] = "windchill",
-    [22] = "CantSprint",
-    [23] = "foodeaten"
+    [MoodleType.Endurance] = "endurance",
+    [MoodleType.Tired] = "tired",
+    [MoodleType.Hungry] = "hungry",
+    [MoodleType.Panic] = "panic",
+    [MoodleType.Sick] = "sick",
+    [MoodleType.Bored] = "bored",
+    [MoodleType.Unhappy] = "unhappy",
+    [MoodleType.Bleeding] = "bleeding",
+    [MoodleType.Wet] = "wet",
+    [MoodleType.HasACold] = "hasacold",
+    --[MoodleType.Angry] = "angry",
+    [MoodleType.Stress] = "stress",
+    [MoodleType.Thirst] = "thirst",
+    [MoodleType.Injured] = "injured",
+    [MoodleType.Pain] = "pain",
+    [MoodleType.HeavyLoad] = "heavyload",
+    [MoodleType.Drunk] = "drunk",
+    [MoodleType.Dead] = "dead",
+    [MoodleType.Zombie] = "zombie",
+    [MoodleType.Hyperthermia] = "hyperthermia",
+    [MoodleType.Hypothermia] = "hypothermia",
+    [MoodleType.Windchill] = "windchill",
+    [MoodleType.CantSprint] = "CantSprint",
+    [MoodleType.FoodEaten] = "foodeaten"
 }
 
 ---@type Moodles
@@ -34,9 +34,6 @@ local old_getMoodleLevel = moodles.getMoodleLevel
 ---@param self Moodles
 ---@param moodleType MoodleType|int
 moodles.getMoodleLevel = function(self, moodleType)
-    if instanceof(moodleType, "MoodleType") then
-        moodleType = MoodleType.ToIndex(moodleType)
-    end
     local luaType = moodleTypeToLuaMoodle[moodleType]
     if not luaType then
         return old_getMoodleLevel(self, moodleType)
